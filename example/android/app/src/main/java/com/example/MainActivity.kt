8d95ab398a726d00388f8ab0e68e136f4bed9bd7
RakaDoank <raka.fauzi@myrepublic.net.id>
2025-07-21 10:01:35 +0700

    fix example

--- example/android/app/src/main/java/com/example/MainActivity.kt
+++ example/android/app/src/main/java/com/example/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -14,13 +15,13 @@ class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "example"
 
   override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(null)
-	}
+	super.onCreate(null)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+	  DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
